71a3791b8e6a85df838c17486c400debfa1e2a65
Victor <joao.z20.godoi.900@gmail.com>
2025-02-27 21:05:36 -0300

    creating game end menu classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame as pg
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pg.init()
+        # Criando a janela do jogo com o parametro Tamanho
+        self.window = pg.display.set_mode(size=(600, 480))
 
-    def run(self, ):
-        pass
+    def run(self):
+        print("Setup start")
+        print("Setup end")
+        print("loop start")
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+            # checando todos os eventos
+            # for event in pg.event.get():
+            #     if event == pg.QUIT:
+            #         pg.quit()  # Fecha a janela do jogo
+            #         quit()  # Fecha o pygame
+
--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import pygame as pg
+from code.game import Game
 
-print("Setup start")
-pg.init()
-
-# Criando a janela do jogo com o parametro Tamanho
-window = pg.display.set_mode(size=(600, 480))
-print("Setup end")
-
-print("loop start")
-while True:
-    # checando todos os eventos
-    for event in pg.event.get():
-        if event == pg.QUIT:
-            pg.quit()  # Fecha a janela do jogo
-            quit()  # Fecha o pygame
+game = Game()
+game.run()
